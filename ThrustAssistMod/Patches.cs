d1c79f561e3a036e28a5cbac3361c8570befca66
Peter Taylor <darthan184@gmail.com>
2024-09-13 15:07:26 +0100

    Bug fixes. Uncontrollable rockets will no longer be affected Will no longer remain on after returning to the build screen, revert to launch or load quick save.

---
+++ ThrustAssistMod/Patches.cs
@@ -0,0 +1,22 @@
+using System.Linq; // contains extensions
+using HarmonyLib; // contains extensions
+
+namespace ThrustAssistMod
+{
+    [HarmonyLib.HarmonyPatch(typeof(SFS.World.GameManager), "RevertToLaunch")]
+     class GameManager_RevertToLaunch
+    {
+        static void Postfix()
+        {
+            ThrustAssistMod.UI.AssistOn=false;
+        }
+    }
+    [HarmonyLib.HarmonyPatch(typeof(SFS.World.GameManager), "LoadSave")]
+     class GameManager_LoadSave
+    {
+        static void Postfix()
+        {
+            ThrustAssistMod.UI.AssistOn=false;
+        }
+    }
+}
